fd4c50a066c05601cd0dce7281623ba10f4a1084
ndljava <ndljava@126.com>
2013-04-26 15:34:22 +0800

    hello

---
+++ HelloWorld/HelloModule/src/org/myorg/action/SomeAction.java
@@ -0,0 +1,31 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.myorg.action;
+
+import com.frame.ui.ArageFrame;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import org.openide.awt.ActionID;
+import org.openide.awt.ActionReference;
+import org.openide.awt.ActionRegistration;
+import org.openide.util.NbBundle.Messages;
+
+@ActionID(
+        category = "Window",
+        id = "com.frame.ui.SomeAction")
+@ActionRegistration(
+        displayName = "#CTL_SomeAction")
+@ActionReference(path = "Menu/Window", position = 3333, separatorAfter = 3383)
+@Messages("CTL_SomeAction=dddd")
+public final class SomeAction implements ActionListener {
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        // TODO implement action body
+        System.out.println("ndljava");
+        new ArageFrame().setVisible(true);
+        
+    }
+}
