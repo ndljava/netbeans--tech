64ccd04278958059ccc25ef1d667d8545aa4a344
ndljava <ndljava@126.com>
2013-05-22 18:34:45 +0800

    解决select--manager2的问题

---
+++ CppApp/main.cpp
@@ -0,0 +1,27 @@
+/* 
+ * File:   main.cpp
+ * Author: Administrator
+ *
+ * Created on May 22, 2013, 6:00 PM
+ */
+
+#include <cstdlib>
+#include <iostream>
+
+using namespace std;
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+    std::cout << "ndljava";
+
+    string b;
+    while (true) {
+        std::cin >> b;
+        std::cout << b;
+    }
+
+    return 0;
+}
+
