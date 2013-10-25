5b816c6cd5af0b0ccc736d636ae7c179f16db291
ndljava <ndljava@126.com>
2013-10-25 21:07:59 +0800

    ok

---
+++ jsv/src/com/cn/file/Filefilter.java
@@ -0,0 +1,37 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.cn.file;
+
+import java.io.File;
+
+/**
+ *
+ * @author Administrator
+ */
+public class Filefilter {
+
+    private String strDir;
+    
+    
+    public Filefilter() {
+    }
+    
+    public void readFils(){
+        
+         
+        
+    }
+    
+    
+    public static void main(String[] args) {
+        
+        
+        
+        
+    }
+    
+    
+    
+}
