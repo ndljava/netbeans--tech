7587e7a0a8a9dc6f88821ede4dce1fa621f5ecfd
ndljava <ndljava@126.com>
2013-10-26 10:57:58 +0800

    ok

--- jsv/src/com/cn/file/Filefilter.java
+++ jsv/src/com/cn/file/Filefilter.java
@@ -5,6 +5,13 @@
 package com.cn.file;
 
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.RandomAccessFile;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 /**
  *
@@ -12,26 +19,120 @@ import java.io.File;
  */
 public class Filefilter {
 
-    private String strDir;
-    
-    
+    private String strDir = "E:\\uitable\\dragon";
+    private String xmlStr;
+    private StringBuffer sbc;
+
     public Filefilter() {
     }
-    
-    public void readFils(){
-        
-         
-        
+
+    public void readFiles(String path) {
+        sbc = new StringBuffer();
+        this.forFiles(strDir);
+
     }
-    
-    
+
+    private void forFiles(String path) {
+
+        File f = new File(path);
+        File[] fs = f.listFiles();
+
+        for (int i = 0; i < fs.length; i++) {
+
+            if (fs[i].isFile() && fs[i].getName().endsWith(".xml") && !fs[i].getName().endsWith("preRes.xml")) {
+                //System.out.println(fs[i].getAbsolutePath());
+                this.readFile(fs[i]);
+
+            } else if (fs[i].isDirectory() && fs[i].getName().indexOf("Table") == -1) {
+                forFiles(fs[i].getAbsolutePath());
+            }
+
+        }
+
+    }
+
+    private void readFile(File f) {
+        try {
+
+            RandomAccessFile raf = new RandomAccessFile(f, "r");
+            String fff;
+            String fpatten;
+            fff = raf.readLine();
+
+            while (fff != null) {
+                if (fff.indexOf("icon") > -1 || fff.indexOf("source") > -1) {
+                    // System.out.println(fff);
+                    // fpatten= Pattern.
+                    Pattern pt = Pattern.compile("(source|icon)=\"(.*?)\"");
+                    Matcher mc = pt.matcher(fff);
+                    while (mc.find()) {
+                        fpatten = mc.group(2);
+                        //System.out.println(mc.groupCount());
+
+                        if (this.xmlStr.indexOf(fpatten) == -1) {
+
+                            if (sbc.indexOf("<res cacheType=\"2\" url=\"" + fpatten + "\"/>") == -1) {
+
+                                sbc.append("<res cacheType=\"2\" url=\"" + fpatten + "\"/>");
+                                System.out.println("<res cacheType=\"2\" url=\"" + fpatten + "\"/>");
+
+                            }
+
+
+                        }
+
+                    }
+
+                }
+
+                fff = raf.readLine();
+            }
+
+            raf.close();
+
+        } catch (FileNotFoundException ex) {
+            Logger.getLogger(Filefilter.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (IOException ex) {
+            Logger.getLogger(Filefilter.class.getName()).log(Level.SEVERE, null, ex);
+        }
+
+    }
+
+    public void readTempFile(String path) {
+
+        System.out.println(path);
+        try {
+
+            RandomAccessFile raf = new RandomAccessFile(new File(path), "r");
+            String fff;
+            fff = raf.readLine();
+
+            StringBuffer sb = new StringBuffer();
+
+            while (fff != null) {
+                sb.append(fff);
+                fff = raf.readLine();
+            }
+
+            this.xmlStr = sb.toString();
+
+            raf.close();
+        } catch (FileNotFoundException ex) {
+            Logger.getLogger(Filefilter.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (IOException ex) {
+            Logger.getLogger(Filefilter.class.getName()).log(Level.SEVERE, null, ex);
+        }
+
+
+    }
+
     public static void main(String[] args) {
-        
-        
-        
-        
+
+        String strDir = "E:\\uitable\\dragon";
+        Filefilter ff = new Filefilter();
+
+        ff.readTempFile("F:\\table\\config\\preRes.xml");
+        ff.readFiles(strDir);
+
     }
-    
-    
-    
 }
