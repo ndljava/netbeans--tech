1736b2f2cd5704e424e1f50faab350beafcce6c7
ndljava <ndljava@126.com>
2014-03-05 13:40:13 +0800

    ok

--- CppApp/main.cpp
+++ CppApp/main.cpp
@@ -20,8 +20,28 @@ int main(int argc, char** argv) {
     string b = "fgthdrthdrtdrt";
 
     std::cout << b;
+
+    int i = 3;
+    switch (i) {
+        case 1:
+            std::cout << i << endl;
+            break;
+        case 2:
+            std::cout << i << endl;
+            break;
+        case 3:
+            std::cout << i << endl;
+            break;
+    }
+
+    CHAR t = 'r';
+    int k=-126;
+    unsigned char p=-126;
     
+    cout<<k<<p<<char(k)<<endl;
     
+    std:: cout << t<<"==="<<sizeof(t) << endl;
+ 
     return 0;
 }
 
